cc61e83c7fdeb061dc4ca980ff6c78dcede44178
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2022-12-31 15:32:04 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
 
 import { Searchbar } from './Searchbar/Searchbar';
@@ -10,97 +10,143 @@ import { Button } from './Button/Button';
 
 const PER_PAGE = 12;
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    searchData: [],
-    error: null,
-    page: 1,
-    largeImgUrl: '',
-    isEmpty: false,
-    isLoading: false,
-    showBtn: false,
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchData, setSearchData] = useState([]);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [largeImgUrl, setLargeImgUrl] = useState('');
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
 
-  async componentDidUpdate(_, prevState) {
-    const { searchQuery, page } = this.state;
-
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+  useEffect(() => {
+    if (!searchQuery) return;
+    const fetchImages = async () => {
       try {
-        this.setState({ isLoading: true });
+        setIsLoading(true);
         const { hits, totalHits } = await getImages(searchQuery, page);
 
         if (hits.length === 0) {
-          this.setState({ isEmpty: true });
+          setIsEmpty(true);
           return;
         }
-        this.setState(prevState => ({
-          searchData: [...prevState.searchData, ...hits],
-          showBtn: page < Math.ceil(totalHits / PER_PAGE),
-        }));
+        setSearchData(prev => [...prev, ...hits]);
+        setShowBtn(page < Math.ceil(totalHits / PER_PAGE));
       } catch (error) {
-        this.setState({ error: error.message });
+        setError(error.message);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
+    };
+    fetchImages();
+  }, [searchQuery, page]);
+
+  const onImageClick = largeImgUrl => {
+    setLargeImgUrl(largeImgUrl);
+  };
+
+  const loadMoreImages = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const onSubmit = searchQuerys => {
+    if (searchQuerys === searchQuery) {
+      alert('same query, try change your request');
+      return;
     }
-  }
-  onImageClick = largeImgUrl => {
-    this.setState({ largeImgUrl });
+    setSearchQuery(searchQuerys);
+    setSearchData([]);
+
+    setPage(1);
   };
 
-  loadMoreImages = () => {
-    this.setState({ page: this.state.page + 1 });
-  };
+  return (
+    <div className={css.app}>
+      <Searchbar onSubmit={onSubmit} />
+      {searchData.length > 0 && (
+        <ImageGallery searchData={searchData} onImageClick={onImageClick} />
+      )}
+      {isLoading && (
+        <div className={css.backdrop}>
+          <Loader />
+        </div>
+      )}
+      {isEmpty && (
+        <p className={css.text}>
+          Nothing was found by the request '{searchQuery}'.
+        </p>
+      )}
+      {error && <p className={css.text}>Something wrong! {error}</p>}
+      {showBtn && <Button loadMoreImages={loadMoreImages} />}
+      {largeImgUrl && (
+        <Modal onImageClick={onImageClick} largeImgUrl={largeImgUrl} />
+      )}
+    </div>
+  );
+};
 
-  onSubmit = searchQuery => {
-    if (searchQuery !== this.state.searchQuery) {
-      this.setState({
-        searchQuery,
-        searchData: [],
-        error: null,
-        page: 1,
-        isEmpty: false,
-        showBtn: true,
-      });
-    }
-  };
+// export class App extends Component {
 
-  render() {
-    const {
-      searchData,
-      searchQuery,
-      error,
-      isEmpty,
-      isLoading,
-      showBtn,
-      largeImgUrl,
-    } = this.state;
-    return (
-      <div className={css.app}>
-        <Searchbar onSubmit={this.onSubmit} />
-        {searchData.length > 0 && (
-          <ImageGallery
-            searchData={searchData}
-            onImageClick={this.onImageClick}
-          />
-        )}
-        {isLoading && (
-          <div className={css.backdrop}>
-            <Loader />
-          </div>
-        )}
-        {isEmpty && (
-          <p className={css.text}>
-            Nothing was found by the request '{searchQuery}'.
-          </p>
-        )}
-        {error && <p className={css.text}>Something wrong! {error}</p>}
-        {showBtn && <Button loadMoreImages={this.loadMoreImages} />}
-        {largeImgUrl && (
-          <Modal onImageClick={this.onImageClick} largeImgUrl={largeImgUrl} />
-        )}
-      </div>
-    );
-  }
-}
+//   async componentDidUpdate(_, prevState) {
+//     const { searchQuery, page } = this.state;
+
+//     if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+//       try {
+//         this.setState({ isLoading: true });
+//         const { hits, totalHits } = await getImages(searchQuery, page);
+
+//         if (hits.length === 0) {
+//           this.setState({ isEmpty: true });
+//           return;
+//         }
+//         this.setState(prevState => ({
+//           searchData: [...prevState.searchData, ...hits],
+//           showBtn: page < Math.ceil(totalHits / PER_PAGE),
+//         }));
+//       } catch (error) {
+//         this.setState({ error: error.message });
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
+//     }
+//   }
+
+//   render() {
+//     const {
+//       searchData,
+//       searchQuery,
+//       error,
+//       isEmpty,
+//       isLoading,
+//       showBtn,
+//       largeImgUrl,
+//     } = this.state;
+//     return (
+//       <div className={css.app}>
+//         <Searchbar onSubmit={this.onSubmit} />
+//         {searchData.length > 0 && (
+//           <ImageGallery
+//             searchData={searchData}
+//             onImageClick={this.onImageClick}
+//           />
+//         )}
+//         {isLoading && (
+//           <div className={css.backdrop}>
+//             <Loader />
+//           </div>
+//         )}
+//         {isEmpty && (
+//           <p className={css.text}>
+//             Nothing was found by the request '{searchQuery}'.
+//           </p>
+//         )}
+//         {error && <p className={css.text}>Something wrong! {error}</p>}
+//         {showBtn && <Button loadMoreImages={this.loadMoreImages} />}
+//         {largeImgUrl && (
+//           <Modal onImageClick={this.onImageClick} largeImgUrl={largeImgUrl} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,47 +1,86 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = {
-    searchQuery: '',
-  };
-  handleInputeChange = e => {
-    this.setState({ searchQuery: e.target.value.toLowerCase() });
-  };
+export const Searchbar = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-  handleSubmit = e => {
+  const handleInputeChange = e => setSearchQuery(e.target.value.toLowerCase());
+
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.searchQuery.trim() === '') {
-      alert('Enter text');
+
+    const normalizedQuery = searchQuery.trim().toLowerCase();
+
+    if (!normalizedQuery) {
+      alert('enter the query');
       return;
     }
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: '' });
+    onSubmit(normalizedQuery);
+    setSearchQuery('');
   };
 
-  render() {
-    return (
-      <header className={css.searchbar}>
-        <form className={css.searchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.searchFormBtn}>
-            <span className={css.searchFormBtnLabel}>Search</span>
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className={css.searchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.searchFormBtn}>
+          <span className={css.searchFormBtnLabel}>Search</span>
+        </button>
 
-          <input
-            className={css.searchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleInputeChange}
-            value={this.state.searchQuery}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.searchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleInputeChange}
+          value={searchQuery}
+        />
+      </form>
+    </header>
+  );
+};
+
+// export class Searchbar extends Component {
+//   state = {
+//     searchQuery: '',
+//   };
+//   handleInputeChange = e => {
+//     this.setState({ searchQuery: e.target.value.toLowerCase() });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     if (this.state.searchQuery.trim() === '') {
+//       alert('Enter text');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.searchQuery);
+//     this.setState({ searchQuery: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.searchbar}>
+//         <form className={css.searchForm} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={css.searchFormBtn}>
+//             <span className={css.searchFormBtnLabel}>Search</span>
+//           </button>
+
+//           <input
+//             className={css.searchFormInput}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={this.handleInputeChange}
+//             value={this.state.searchQuery}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
